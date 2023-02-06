e0c13574da3cb69b0d82cebd94e07745ca28cda4
JJGAvery <116852587+JJGAvery@users.noreply.github.com>
2023-02-05 21:05:39 -0700

    filled out html file with bootstrap. added functions to the javascript file, linked the api url, and set up the api key. tested the functions to verify the api will retrieve weather data based on which location was entered. checked the console to verify the data was stored into local storage and displays the required information. temp, weather icon, wind speed and humidity are the required points that will be needed to be displayed in the current weather section and the 5 day forecast section.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,1 +1,50 @@
 console.log(window);
+
+const APIKey = "25213c85f2dcb742bd77c26242a111f7";
+const baseUrl = 'https://api.openweathermap.org/';
+
+// const now = dayjs();
+const cityNameInput = document.querySelector("#cityName");
+const searchForm = document.querySelector("#searchForm");
+const searchButton = document.querySelector(".btn");
+const previousSearches = document.querySelector("#previousSearches");
+const previousSearchContainer = document.querySelector("#previousSearches .cardBody");
+const dailyCardContainer = document.querySelector("#currentForecast");
+
+searchButton.addEventListener('click', (e) =>{
+    e.preventDefault()
+    currentForecast(cityNameInput.value);
+})
+
+function currentForecast(cityNameInput){
+    const url = `${baseUrl}data/2.5/weather?q=${cityNameInput}&APPID=${APIKey}&units=imperial`;
+    fetch(url)
+    .then((response) => {
+        console.log(response);
+        return response.json();
+    }).then((data) => {
+        console.log(data)
+        setToLocalStorage(data);
+        displayCurrent(data);
+    })
+};
+    
+function setToLocalStorage(data){
+    const { 
+        name,
+        main: {
+            humidity, 
+            temp, 
+        },
+        wind: {
+            speed: windSpeed
+        },
+        weather: {
+            0: {icon}
+        }
+    } = data
+    localStorage.setItem(name, JSON.stringify({humidity, temp, windSpeed, icon}));  
+}
+
+function displayCurrent(data) {
+}
