c33a51219223dd0cb405e98c8deaeab10dfaeeb5
JJGAvery <116852587+JJGAvery@users.noreply.github.com>
2023-02-19 21:26:23 -0700

    styled css to properly display the 5 day forecast cards. created a function to display recent searches when the page initially loads. styled the previous search text to change color when hovered over and to be clicked on and display a previous search in the current weather and 5 day forecast. updated the readme file. added a screenshot of the website to the images file and linked it to the readme.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,90 +1,108 @@
-console.log(window);
-
 const APIKey = "25213c85f2dcb742bd77c26242a111f7";
 const baseUrl = 'https://api.openweathermap.org/';
 
 const cityNameInput = document.querySelector("#cityName");
 const searchForm = document.querySelector("#searchForm");
 const searchButton = document.querySelector(".btn");
-const previousSearches = document.querySelector("#previousSearcheField");
-const previousSearchContainer = document.querySelector("#previousSearchField .cardBody");
+const previousSearchContainer = document.querySelector("#previousSearchField");
+let previousSearchName;
 const dailyCardContainer = document.querySelector("#currentForecast");
-const previousField = document.querySelector("#previousSearchField")
+
+loadSearches();
 
 searchButton.addEventListener('click', (e) =>{
     e.preventDefault()
     currentForecast(cityNameInput.value);
 })
 
+function loadSearches(){
+    previousSearch();
+    previousSearchName = document.querySelectorAll('#city')
+    previousSearchName.forEach(cityName => cityName.addEventListener('click', (e)=>loadPrevious(e)))
+}
+
 function currentForecast(cityNameInput){
     const url = `${baseUrl}data/2.5/weather?q=${cityNameInput}&APPID=${APIKey}&units=imperial`;
     fetch(url)
-    .then((response) => {
-        console.log(response);
+    .then((response) => {   
         return response.json();
     }).then((data) => {
-        clearcontent();
+        clearCurrent();
         setToLocalStorage(data);
         displayCurrent(data);
         fiveDayForecast(data);
+        loadSearches();
     })
+    
 };
 
+// load previous searches on website load
+function loadPrevious(e){
+    const cityName = e.target.dataset.city;
+    const cityData = JSON.parse(localStorage.getItem(cityName));
+    clearCurrent();
+    displayCurrent(cityData);
+    fiveDayForecast(cityData);
+}
+
+// previous search history
+function previousSearch(){
+    const storedCities = Object.keys(localStorage)
+    const storedCitiesHTML = storedCities.map((city) => {
+        return (
+            `<li id="city">\
+                <h3 class="previous-search-city" data-city="${city}">${city}</h3>
+            </li>\
+            `
+        )
+    })
+    previousSearchContainer.innerHTML=storedCitiesHTML.join(' ')    
+};
+
+
 function fiveDayForecast(data){
-    console.log(data)
     const url = `${baseUrl}data/2.5/forecast?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=${APIKey}&units=imperial`;
     fetch(url)
     .then((response) => {
-        console.log(response);
         return response.json();
     }).then ((fiveDayData) => {
-        console.log(fiveDayData)
         displayFiveDayForecast(fiveDayData);
     })
 };
     
+// current weather
 function displayCurrent(data){
     document.getElementById("conditions");
-    conditions.innerHTML+=`<h4>${data.name}</h4>`;
+    conditions.innerHTML+=`<h2>${data.name}</h2>`;
     conditions.innerHTML+=`<p><img src = "http://openweathermap.org/img/wn/${data.weather[0].icon}.png"></p>`;
     conditions.innerHTML+=`<p>Temp: ${data.main.temp}°F</p>`;
     conditions.innerHTML+=`<p>Wind Speed: ${data.wind.speed}MPH</p>`;
     conditions.innerHTML+=`<p>Humidity: ${data.main.humidity}%</p>`;
-}
+};
+
 // 5 day forecast
 function displayFiveDayForecast(fiveDayData){
-    // day1
-    document.getElementById("day1");
-    day1.innerHTML+=`<p><img src = "http://openweathermap.org/img/wn/${fiveDayData.list[1].weather[0].icon}.png"></p>`;
-    day1.innerHTML+=`<p>Temp: ${fiveDayData.list[1].main.temp_max}°F</p>`;
-    day1.innerHTML+=`<p>Wind Speed: ${fiveDayData.list[1].wind.speed}MPH</p>`;
-    day1.innerHTML+=`<p>Humidity: ${fiveDayData.list[1].main.humidity}%</p>`;
-    // day2
-    document.getElementById("day2");
-    day2.innerHTML+=`<p><img src = "http://openweathermap.org/img/wn/${fiveDayData.list[9].weather[0].icon}.png"></p>`;
-    day2.innerHTML+=`<p>Temp: ${fiveDayData.list[9].main.temp_max}°F</p>`;
-    day2.innerHTML+=`<p>Wind Speed: ${fiveDayData.list[9].wind.speed}MPH</p>`;
-    day2.innerHTML+=`<p>Humidity: ${fiveDayData.list[9].main.humidity}%</p>`;
-    // day3
-    document.getElementById("day3");
-    day3.innerHTML+=`<p><img src = "http://openweathermap.org/img/wn/${fiveDayData.list[17].weather[0].icon}.png"></p>`;
-    day3.innerHTML+=`<p>Temp: ${fiveDayData.list[17].main.temp_max}°F</p>`;
-    day3.innerHTML+=`<p>Wind Speed: ${fiveDayData.list[17].wind.speed}MPH</p>`;
-    day3.innerHTML+=`<p>Humidity: ${fiveDayData.list[17].main.humidity}%</p>`;
-    // day4
-    document.getElementById("day4");
-    day4.innerHTML+=`<p><img src = "http://openweathermap.org/img/wn/${fiveDayData.list[25].weather[0].icon}.png"></p>`;
-    day4.innerHTML+=`<p>Temp: ${fiveDayData.list[25].main.temp_max}°F</p>`;
-    day4.innerHTML+=`<p>Wind Speed: ${fiveDayData.list[25].wind.speed}MPH</p>`;
-    day4.innerHTML+=`<p>Humidity: ${fiveDayData.list[25].main.humidity}%</p>`;
-    // day5
-    document.getElementById("day5");
-    day5.innerHTML+=`<p><img src = "http://openweathermap.org/img/wn/${fiveDayData.list[33].weather[0].icon}.png"></p>`;
-    day5.innerHTML+=`<p>Temp: ${fiveDayData.list[33].main.temp_max}°F</p>`;
-    day5.innerHTML+=`<p>Wind Speed: ${fiveDayData.list[33].wind.speed}MPH</p>`;
-    day5.innerHTML+=`<p>Humidity: ${fiveDayData.list[33].main.humidity}%</p>`;
-}
+    const fiveDayContainer = document.getElementById("five_day_data")
+    const fiveDayTrim = fiveDayData.list.filter((forecast) => {
+        return forecast.dt_txt.includes("12:00:00")
+    })
+    const fiveDayCards = fiveDayTrim.map((day, i) => {
+        const dateFormat = new Date(day.dt_txt).toDateString()
+        return (
+            `<li class="five_day_card" id="day${i+1}">\
+                <h3 class="date"> ${dateFormat}</h3>
+                <p><img src = "http://openweathermap.org/img/wn/${day.weather[0].icon}.png"></p>\
+                <p>Temp: ${day.main.temp_max}°F</p>\
+                <p>Wind Speed: ${day.wind.speed}MPH</p>\
+                <p>Humidity: ${day.main.humidity}%</p>\
+            </li>\
+            `
+        )
+    })
+    fiveDayContainer.innerHTML = fiveDayCards.join(' ')
+};
 
+// set weather conditions to local storage
 function setToLocalStorage(data){
     const { 
         name,
@@ -97,21 +115,17 @@ function setToLocalStorage(data){
             temp, 
         },
         wind: {
-            speed: windSpeed
+            speed
         },
         weather: {
             0: {icon}
         }
     } = data
-    localStorage.setItem(name, JSON.stringify({lat, lon, humidity, temp, windSpeed, icon})); 
-}
+    localStorage.setItem(name, JSON.stringify({name, coord: {lat, lon}, main: {humidity, temp}, wind: {speed}, weather: {0: {icon}}})); 
+};
 
-
-function clearcontent() {
+// clear previous data for current weather and for 5 day forecast
+function clearCurrent() {
     document.getElementById("conditions").innerHTML = "";
-}
-
-
-
-
-
+    document.getElementById("five_day_data").innerHTML = "";
+};
--- README.md
+++ README.md
@@ -1,15 +1,18 @@
 # weather-dashboard
 
 ## **Link to deployed application**
+https://jjgavery.github.io/weather-dashboard
 
 
 ## **Description**  
-
+In this challenge I was instructed to make a webpage linking an api to display the current weather and a 5 day forecast for a city that is input by a user.
 
 ## **Technologies**
 - HTML
 - CSS
 - JAVASCRIPT
+- https://openweathermap.org/api
+- https://openweathermap.org/api/geocoding-api
 
 ## **Preview**
-![weather-dashboard](./) 
+![weather-dashboard](./Assets/images/weather-dashboard_screenshot.png) 
